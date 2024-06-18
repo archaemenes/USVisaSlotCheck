e5b609846ecd2383a270a6be48da4db4d39e07bb
Anisha Mhatre <50442176+archaemenes@users.noreply.github.com>
2024-06-18 15:27:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@ import os
 import requests
 FILE_PATH='/home/archaemenes/software/automation/sendText.txt'
 
+chat_id='6197842445' # Add your personal chat_id here
+chat_id_group='-4269971893' # Add group chat_id
+
 def sendreq(text):
     for t in text.split('\n'):
-        base_url='https://api.telegram.org/bot6423408141:AAGT4idvbIBAyI71NVGLW19my03neku8Ki0/sendMessage?chat_id=6197842445&text={}'.format(t)
-        group_url='https://api.telegram.org/bot6423408141:AAGT4idvbIBAyI71NVGLW19my03neku8Ki0/sendMessage?chat_id=-4269971893&text={}'.format(t)
+        base_url= f"https://api.telegram.org/bot6423408141:AAGT4idvbIBAyI71NVGLW19my03neku8Ki0/sendMessage?chat_id={chat_id}&text={t}"
+        #group_url= f"https://api.telegram.org/bot6423408141:AAGT4idvbIBAyI71NVGLW19my03neku8Ki0/sendMessage?chat_id={chat_id_group}&text={t}"
         print("#_NotificationSent_#")
-        requests.get(group_url)
+        #requests.get(group_url)
         requests.get(base_url)
-#-4269971893
 if __name__ == '__main__':    
     if os.path.isfile(FILE_PATH) and os.stat(FILE_PATH).st_size != 0:
         with open(FILE_PATH, 'r') as file:
